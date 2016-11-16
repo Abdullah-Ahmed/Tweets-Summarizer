608b974b95cb2251dc6881637c56e70e27aac035
Abdullah-Ahmed <AbdullahAhmed016@gmail.com>
2016-11-16 19:23:30 +0200

    Adding User profile details when clicking on his picture

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # README
 
 This README would normally document whatever steps are necessary to get the
@@ -25,4 +24,5 @@ Things you may want to cover:
 * ...
 =======
 # Tweets-Summarizer
->>>>>>> bc7fa64074f7722f8fa09e7ba690aa8f1f2aa575
+
+Its website to filter and sort your timeline depending on selected users 
--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,17 +1,11 @@
 require 'twitter'
 
 class TweetController < ApplicationController
-  @@client ||= Twitter::REST::Client.new do |config|
-    config.consumer_key        = "YOUR_CONSUMER_KEY"
-    config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-    config.access_token        = "YOUR_ACCESS_TOKEN"
-    config.access_token_secret = "YOUR_ACCESS_SECRET"
-  end
-
   def index
+    client = TweetController.create_client
     begin
-      @friends = @@client.friends
-      @tweets = get_tweets()
+      @friends = client.friends
+      @tweets = get_tweets(client)
     rescue => e
       #TODO: render 404 with the error
        puts "Error : #{e.to_s}"
@@ -20,12 +14,12 @@ class TweetController < ApplicationController
 
 
   private
-  def get_tweets
+  def get_tweets(client)
     tweets =
       if params.key?("selected_users") && params["selected_users"].is_a?(Array)
-        @@client.search(get_query(params["selected_users"]), :result_type => "recent").collect
+        client.search(get_query(params["selected_users"]), :result_type => "recent").collect
       else
-        @@client.home_timeline(count: 100)
+        client.home_timeline(count: 100)
       end
   end
 
@@ -39,5 +33,14 @@ class TweetController < ApplicationController
       end
     end
    q
+  end
+
+  def self.create_client
+    @@client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = "YOUR_CONSUMER_KEY"
+      config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+      config.access_token        = "YOUR_ACCESS_TOKEN"
+      config.access_token_secret = "YOUR_ACCESS_SECRET"
+    end
   end
 end
